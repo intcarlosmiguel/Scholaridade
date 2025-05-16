18b4289ad686cb775583656673e08c7b881afc1f
Carlos Miguel Moreira Gonçalves <77444804+intcarlosmiguel@users.noreply.github.com>
2025-05-15 23:33:43 -0300

    Coleta de Artigos
    Coleta de Artigos

--- sci.py
+++ sci.py
@@ -12,11 +12,12 @@ if not os.path.exists("artigos"):
     os.makedirs("artigos")
 
 lista = []
+lista_pdf = []
 link = "https://sci-hub.se/"
 with sync_playwright() as p:
     # Cria um navegador
     
-    browser = p.chromium.launch(headless=False)  # headless=False para visualizar a execução
+    browser = p.chromium.launch(headless=True)  # headless=False para visualizar a execução
     page = browser.new_page()
     for titulo in tqdm(artigos["titulo"].values):
         # Navega até o site do Google   
@@ -30,7 +31,7 @@ with sync_playwright() as p:
 
         # Pressiona Enter
         page.press(xpath, "Enter")
-        page.wait_for_timeout(5000)
+        page.wait_for_timeout(1000)
         try:
             pdf_url = page.query_selector('embed[type="application/pdf"]').get_attribute('src')
             pdf_url = link + pdf_url
@@ -40,13 +41,7 @@ with sync_playwright() as p:
             titulo = titulo.replace(";", "_").replace(",", "_").replace("'", "_").replace('"', "_")
             titulo = titulo.lower()
             if pdf_url:
-                response = requests.get(pdf_url)
-                if response.status_code == 200:
-                    with open(f"./artigos/{titulo}.pdf", "wb") as f:
-                        f.write(response.content)
-                    size += 1
-                else:
-                    print(f"Erro ao baixar o PDF. Status code: {response.status_code}")
+                lista_pdf.append(pdf_url)
         except Exception as e:
             lista.append(titulo)
     
@@ -56,6 +51,8 @@ with sync_playwright() as p:
     with open("falied_artigos/falied_artigos.txt", "w") as f:
         for item in lista:
             f.write("%s\n" % item)
-    
+    with open("artigos/artigos.txt", "w") as f:
+        for item in lista_pdf:
+            f.write("%s\n" % item)
     
     browser.close()
